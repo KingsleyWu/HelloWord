49a94be21307440c7c9f8ffb7cd7573cfe74fa6d
kingsley <351054820@qq.com>
2021-08-05 23:32:26 +0800

    复制内容 （#0）

--- tetriscompose/src/main/java/com/kingsley/compose/tetris/MainActivity.kt
+++ tetriscompose/src/main/java/com/kingsley/compose/tetris/MainActivity.kt
@@ -3,36 +3,116 @@ package com.kingsley.compose.tetris
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kingsley.compose.tetris.logic.*
+import com.kingsley.compose.tetris.ui.GameBody
+import com.kingsley.compose.tetris.ui.GameScreen
+import com.kingsley.compose.tetris.ui.PreviewGamescreen
+import com.kingsley.compose.tetris.ui.combinedClickable
 import com.kingsley.compose.tetris.ui.theme.HelloWordTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        StatusBarUtil.transparentStatusBar(this)
+        SoundUtil.init(this)
+
         setContent {
             HelloWordTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+                    val viewModel = viewModel<GameViewModel>()
+                    val viewState by viewModel.viewState.collectAsState()
+
+                    LaunchedEffect(key1 = Unit) {
+                        while (isActive) {
+                            delay(650L - 55 * (viewState.level - 1))
+                            viewModel.dispatch(Action.GameTick)
+                        }
+                    }
+
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    DisposableEffect(key1 = Unit) {
+                        val observer = object : LifecycleObserver {
+                            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+                            fun onResume() {
+                                viewModel.dispatch(Action.Resume)
+                            }
+
+                            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+                            fun onPause() {
+                                viewModel.dispatch(Action.Pause)
+                            }
+                        }
+                        lifecycleOwner.lifecycle.addObserver(observer)
+                        onDispose {
+                            lifecycleOwner.lifecycle.removeObserver(observer)
+                        }
+
+                    }
+
+
+                    GameBody(combinedClickable(
+                        onMove = { direction: Direction ->
+                            if (direction == Direction.Up) viewModel.dispatch(Action.Drop)
+                            else viewModel.dispatch(Action.Move(direction))
+                        },
+                        onRotate = {
+                            viewModel.dispatch(Action.Rotate)
+                        },
+                        onRestart = {
+                            viewModel.dispatch(Action.Reset)
+                        },
+                        onPause = {
+                            if (viewModel.viewState.value.isRuning) {
+                                viewModel.dispatch(Action.Pause)
+                            } else {
+                                viewModel.dispatch(Action.Resume)
+                            }
+                        },
+                        onMute = {
+                            viewModel.dispatch(Action.Mute)
+                        }
+                    )) {
+                        GameScreen(
+                            Modifier.fillMaxSize()
+                        )
+                    }
                 }
             }
         }
     }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        SoundUtil.release()
+    }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
+@ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloWordTheme {
-        Greeting("Android")
+        GameBody {
+            PreviewGamescreen(Modifier.fillMaxSize())
+        }
     }
 }
---
+++ tetriscompose/src/main/java/com/kingsley/compose/tetris/ui/LedNumber.kt
@@ -0,0 +1,121 @@
+package com.kingsley.compose.tetris.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kingsley.compose.tetris.logic.LedFontFamily
+import com.kingsley.compose.tetris.ui.theme.BrickMatrix
+import com.kingsley.compose.tetris.ui.theme.BrickSpirit
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
+
+@Composable
+fun LedClock(modifier: Modifier = Modifier) {
+
+    val animateValue by rememberInfiniteTransition().animateFloat(
+        initialValue = 0f, targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart,
+        ),
+    )
+
+    var clock by remember { mutableStateOf(0 to 0) }
+
+    DisposableEffect(key1 = animateValue.roundToInt()) {
+        @SuppressLint("SimpleDateFormat")
+        val dateFormat: DateFormat = SimpleDateFormat("H,m")
+        val (curHou, curMin) = dateFormat.format(Date()).split(",")
+        clock = curHou.toInt() to curMin.toInt()
+        onDispose { }
+    }
+
+    Row(modifier) {
+        LedNumber(num = clock.first, digits = 2, fillZero = true)
+
+        val LedComma: @Composable (color: Color) -> Unit = remember {
+            {
+                Text(
+                    ":",
+                    fontFamily = LedFontFamily,
+                    textAlign = TextAlign.End,
+                    color = it,
+                    fontSize = 16.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .width(6.dp)
+                .padding(end = 1.dp),
+        ) {
+
+            LedComma(BrickMatrix)
+            if (animateValue.roundToInt() == 1) {
+                LedComma(BrickSpirit)
+            }
+
+        }
+
+        LedNumber(num = clock.second, digits = 2, fillZero = true)
+    }
+
+}
+
+@Composable
+fun LedNumber(
+    modifier: Modifier = Modifier,
+    num: Int,
+    digits: Int,
+    fillZero: Boolean = false
+) {
+    val textSize = 16.sp
+    val textWidth = 8.dp
+    Box(modifier) {
+        Row(modifier = Modifier.align(Alignment.CenterEnd)) {
+            repeat(digits) {
+                Text(
+                    "8",
+                    fontSize = textSize,
+                    color = BrickMatrix,
+                    fontFamily = LedFontFamily,
+                    modifier = Modifier.width(textWidth),
+                    textAlign = TextAlign.End
+
+                )
+            }
+
+        }
+        Row(
+            modifier = Modifier.align(Alignment.CenterEnd),
+        ) {
+            val str = if (fillZero) String.format("%0${digits}d", num) else num.toString()
+            str.iterator().forEach {
+                Text(
+                    it.toString(),
+                    fontSize = textSize,
+                    color = BrickSpirit,
+                    fontFamily = LedFontFamily,
+                    modifier = Modifier.width(textWidth),
+                    textAlign = TextAlign.End
+
+                )
+            }
+
+        }
+
+    }
+}
